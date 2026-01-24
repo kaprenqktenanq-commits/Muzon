c043475d3de739d53b3262d795bf8108a531553f
sqenaah <sqenaah@github.com>
2026-01-24 15:00:24 +0400

    update

--- AnonXMusic/core/git.py
+++
@@ -1,9 +0,0 @@
-import shutil
-from ..logging import LOGGER
-
-
-def git():
-    if shutil.which("git"):
-        LOGGER(__name__).info("Git Client Found [VPS DEPLOYER]")
-    else:
-        LOGGER(__name__).warning("Git Client Not Found")

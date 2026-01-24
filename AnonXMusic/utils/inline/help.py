2be38a32f19ad7c676fcc520aab01414a385f0c1
sqenaah <sqenaah@github.com>
2026-01-24 20:12:17 +0400

    Remove admin buttons (Broadcast, Bl-Chat, Bl-User, G-Ban, Loop, Maintenance) from help menu

--- AnonXMusic/utils/inline/help.py
+++ AnonXMusic/utils/inline/help.py
@@ -24,37 +24,11 @@ def help_pannel(_, is_sudo, START: Union[bool, int] = None):
                     text=_["H_B_2"],
                     callback_data="help_callback hb2",
                 ),
-                InlineKeyboardButton(
-                    text=_["H_B_3"],
-                    callback_data="help_callback hb3",
-                ),
             ],
             [
-                InlineKeyboardButton(
-                    text=_["H_B_4"],
-                    callback_data="help_callback hb4",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_5"],
-                    callback_data="help_callback hb5",
-                ),
                 InlineKeyboardButton(
                     text=_["H_B_6"],
                     callback_data="help_callback hb6",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_7"],
-                    callback_data="help_callback hb7",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_8"],
-                    callback_data="help_callback hb8",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_9"],
-                    callback_data="help_callback hb9",
                 ),
             ],
             [
